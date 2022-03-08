cb016dc891a79ef60674d21472169b45c4ef8a97
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-03-08 23:55:09 +0700

    creating an action field on class 'snake'

--- Project1/snake.h
+++ Project1/snake.h
@@ -8,9 +8,15 @@ private:
 	std::string colorSnake;
 	int width;
 	int height;
+	char** field;
 public:
 	bool ñreate_a_field(int width_enter = 69, int height_enter = 26) {
 		width = width_enter; height = height_enter;
+		field = new char*[width];
+		
+		for (int i = 0; i < width; i++) {
+			field[i] = new char[height];
+		}
 
 
 	}
